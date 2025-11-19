7c7b51a1c1551e6f72c1a00ce18f98aa0ff4e0c5
Samson Mathews <samsonmmathews@gmail.com>
2025-11-19 12:38:43 -0500

    Updated hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,16 @@ import './hero.css'
 const Hero = () => {
     return(
         <section id="hero-container">
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap');
+            </style>
             <img id='hero-image' src="/Hero_background.svg" alt="" />
+
+            <div id="hero-content">
+                <h1 id="heading">30,000 SQFT</h1>
+                <p id="text">OF AWESOME</p>
+                <button id="submitBtn" type="submit">BOOK NOW</button>
+            </div>
         </section>
     );
 }
