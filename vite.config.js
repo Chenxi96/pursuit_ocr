68a77c6b98817ac3574f0ace95fee2642bfccac3
Chenxi Lin <lin.chenxi14@gmail.com>
2025-11-19 14:17:44 -0500

    Add netlify

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
+import netlify from "@netlify/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),netlifyPlugin()],
+  plugins: [
+    react(),
+    netlifyPlugin(),
+    netlify()
+  ],
   
 })
