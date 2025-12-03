ff18ee369e53fff8ae01b607de3b3239aecec8ab
Samson Mathews <samsonmmathews@gmail.com>
2025-12-03 12:36:50 -0500

    Updated banner images and removed the titles

--- src/components/OurSpace/index.jsx
+++ src/components/OurSpace/index.jsx
@@ -12,19 +12,19 @@ const OurSpace = () => {
             <main>
                 <h2 className="header">Our Space</h2>
                 <Map />
-                <Banner title={"The Track"} image={"/background2.svg"} />
+                <Banner image={"/background2.svg"} />
                 <Section 
                     description={"Get ready to unleash your inner racer on Pursuit OCR’s epic drift tricycle track — a wild, pedal-powered ride that feels straight out of Mario Kart! This massive indoor course stretches the length of three football fields, winding and twisting beneath the obstacle course above. You’ll be drifting around corners, racing your friends, and laughing nonstop as you battle for bragging rights. It’s fast, chaotic, and totally unforgettable — the ultimate grown-up playground experience."} 
                     points={["Drift around corners like a pro no license required!","Battle your friends for the ultimate bragging rights!", "Feels like real-life Mario Kart minus the banana peels!"]}
                     image={"/theTrack_image.svg"}
                 />
-                <Banner title={"The Ball Pit"} image={"/Background.svg"}/>
+                <Banner image={"/Background.svg"}/>
                 <Section 
                     description={"The ball pit at OCR Pursuit is no ordinary playground feature — it’s a massive, four-and-a-half-foot-deep sea of over 100,000 balls that feels more like an endurance challenge than a casual dip. The moment you jump in, you’re swallowed up by the colourful chaos, struggling to stay afloat as you wade through the endless layers. It’s fun, disorienting, and just a little terrifying — especially when you realize how hard it is to get out. Definitely worth trying once, but keep your phone (and maybe your shoes) far away unless you want to lose them to the depths forever."}
                     points={["You might lose your shoes, but you’ll gain an unforgettable story!", "Feels like swimming and sinking at the same time!", "A hilarious full-body workout you never saw coming!"]}
                     image={"/ballPit_image.svg"}
                 />
-                <Banner title={"The Cove Wall"} image={"/ourSpace_banner.svg"} />
+                <Banner image={"/ourSpace_banner.svg"} />
                 <Section 
                     description={"The Cove Wall at Pursuit OCR is a thrilling challenge that feels straight out of an action movie. With its towering curved ramp and glowing neon cubes floating overhead, it’s designed to test your speed, strength, and determination. You’ll feel the rush as you sprint toward the wall, push off, and try to reach the top a perfect mix of athleticism and adrenaline. The vibrant lights and bold design make it not only an exciting obstacle to conquer but also a spot that practically begs for a victory photo once you’ve made it to the top."}
                     points={["Neon lights and cube art make it feel like you’ve stepped into a sci-fi adventure", "Whether you conquer it on the first try or the fifth, you’ll leave grinning—and maybe a little breathless.", "Great for grip, balance, and courage (and an epic photo opportunity!)"]}
