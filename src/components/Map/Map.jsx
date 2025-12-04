f95c5c884566e312331d83ca2bf4dc34a4a2afae
Chenxi Lin <lin.chenxi14@gmail.com>
2025-12-03 21:02:21 -0500

    Add transition styles

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -11,9 +11,16 @@ const Map = () => {
     return(
         <section id="map_section-container">
             <section className='map-container'>
+                <section id='initial-page' className='map-wrapper' style={{left: !isHoveredTrack && !isHoveredBallPit && !isHoveredLava ? '50px' : '-1300px'}}>
+                    <div className='map-description'>
+                        <p>Introducing our Interactive Zones Map! </p>
+                        <p>Find all obstacles, understand the rules and strategies for playing them, and uncover additional information to make your visit more engaging and fun.</p>
+                    </div>
+                </section>
                 <section id='track-section' className='map-wrapper' style={{left: isHoveredTrack ? '50px' : '-13000px'}}>
                     <h2 className='map-heading'>
                         <img src="/interactive_map/TrikeTrackLogo 1.svg" alt="" />
+                        <img src="/interactive_map/cam.svg" alt="" />
                     </h2>
                     <div className='map-description'>
                         <p>Race, drift, and laugh nonstop on Pursuit OCR’s massive, twisty drift tricycle track — the ultimate grown-up playground!</p>
@@ -28,6 +35,7 @@ const Map = () => {
                 <section id='ballpit-section' className='map-wrapper' style={{left: isHoveredBallPit ? '50px' : '-13000px'}}>
                     <h2 className='map-heading'>
                         <img src="/interactive_map/BallPitLogo.svg" alt="" />
+                        <img src="/interactive_map/cam.svg" alt="" />
                     </h2>
                     <div className='map-description'>
                         <p>Dive, aim, and scramble in Pursuit OCR’s massive 100,000-ball pit — now featuring moving projection-mapped targets for even more chaotic fun!</p>
@@ -42,6 +50,7 @@ const Map = () => {
                 <section id='floorLava-section' className='map-wrapper' style={{left: isHoveredLava ? '50px' : '-13000px'}}>
                     <h2 className='map-heading'>
                         <img src="/interactive_map/FloorIsLavaLogo.svg" alt="" />
+                        <img src="/interactive_map/cam.svg" alt="" />
                     </h2>
                     <div className='map-description'>
                         <p>Jump, balance, and dodge the heat in Pursuit OCR’s projection-mapped Floor Is Lava — a living, glowing video-game challenge under your feet!</p>
@@ -55,36 +64,40 @@ const Map = () => {
                 </section>
             </section>
             <section id="map-container">
-
-                <img id='ball-pit' className='zone' src="/interactive_map/Ball Pit 1.svg" alt="" onMouseEnter={() => {
-                    setIsHoveredBallPit(true)
-                    setIsHoveredLava(false)
-                    setIsHoveredTrack(false)
-                }} />
+                <a id='ball-pit' href="">
+                    <img className='zone' src="/interactive_map/Ball Pit 1.svg" alt="" onMouseEnter={() => {
+                        setIsHoveredBallPit(true)
+                        setIsHoveredLava(false)
+                        setIsHoveredTrack(false)
+                    }} />
+                </a>
+                
 
                 <img id='cave-wall' className='zone' src="/interactive_map/Cave 1.svg" alt="" />
 
 
                 <img id='cove-wall' className='zone' src="/interactive_map/Cove 1.svg" alt="" />
 
-
-                <img id='floor-lava' className='zone' src="/interactive_map/Lava 1.svg" alt="" onMouseEnter={() => {
-                    setIsHoveredLava(true)
-                    setIsHoveredTrack(false)
-                    setIsHoveredBallPit(false)
-                }}  />
+                <a id='floor-lava' href="">
+                    <img className='zone' src="/interactive_map/Lava 1.svg" alt="" onMouseEnter={() => {
+                        setIsHoveredLava(true)
+                        setIsHoveredTrack(false)
+                        setIsHoveredBallPit(false)
+                    }}  />
+                </a>
 
                 <img id='ninja-path' className='zone' src="/interactive_map/Ninja  Path 1.svg" alt="" />
 
 
                 <img id='party-deck' className='zone' src="/interactive_map/Party Deck 1.svg" alt="" />
 
-
-                <img id='track' className='zone' src="/interactive_map/Track 1.svg" alt="" onMouseEnter={() => {
-                    setIsHoveredTrack(true)
-                    setIsHoveredLava(false)
-                    setIsHoveredBallPit(false)
-                }}  />
+                <a id='track' href="">
+                    <img className='zone' src="/interactive_map/Track 1.svg" alt="" onMouseEnter={() => {
+                        setIsHoveredTrack(true)
+                        setIsHoveredLava(false)
+                        setIsHoveredBallPit(false)
+                    }}  />
+                </a>
 
                 <img id='ninja-warrior' className='zone' src="/interactive_map/Warrior 1.svg" alt="" />
 
